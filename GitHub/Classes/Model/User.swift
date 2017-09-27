f50cf0de8e518e30df945ce92fa67b70a82a3748
Mathias Quintero <me@quintero.io>
2017-09-27 02:51:18 +0200

    Splitting user

--- GitHub/Classes/Model/User.swift
+++ GitHub/Classes/Model/User.swift
@@ -1,5 +1,5 @@
 //
-//  Permissions.swift
+//  User.swift
 //  GitHub
 //
 //  Created by Mathias Quintero on 9/2/17.
@@ -7,48 +7,65 @@
 
 import Sweeft
 
-public struct User: Codable {
+public struct User: APIObjectWithDetail {
+    
+    public struct Basic: APIBasic {
+        public let id: String
+        public let type: UserType
+        public let avatar: URL?
+        
+        public enum CodingKeys: String, CodingKey {
+            case id = "login"
+            case type
+            case avatar = "avatar_url"
+        }
+    }
     
     public enum UserType: String, Codable {
         case user = "User"
     }
     
-    public let type: UserType
-    public let id: String
-    public let name: String?
-    public let avatar: URL?
-    
-    public let bio: String?
-    
-    public let company: String?
-    public let blog: URL?
-    public let location: String?
-    
-    public let followers: Int?
-    public let following: Int?
-    
-    public let hireable: Bool?
-    
-    public let created: Date?
-    public let updated: Date?
-    
-    public enum CodingKeys: String, CodingKey {
-        case id = "login"
-        case type
-        case name
-        case avatar = "avatar_url"
-        case bio
-        case company
-        case blog
-        case location
+    public struct Detail: Codable {
         
-        case followers
-        case following
+        public let name: String?
+        public let bio: String?
         
-        case hireable
+        public let company: String?
+        public let blog: URL?
+        public let location: String?
         
-        case created = "created_at"
-        case updated = "updated_at"
+        public let followers: Int
+        public let following: Int
+        
+        public let hireable: Bool
+        
+        public let created: Date
+        public let updated: Date
+        
+        public enum CodingKeys: String, CodingKey {
+            case name
+            
+            case bio
+            case company
+            case blog
+            case location
+            
+            case followers
+            case following
+            
+            case hireable
+            
+            case created = "created_at"
+            case updated = "updated_at"
+        }
+    }
+    
+    public let basic: Basic
+    public let detail: Detail?
+    
+    public init(basic: Basic, detail: Detail?) {
+        self.basic = basic
+        self.detail = detail
     }
     
 }
