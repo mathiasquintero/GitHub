cffb1acc863a69c3b276d3a7dcfc17a08adeefc0
Mathias Quintero <me@quintero.io>
2017-09-27 11:07:24 +0200

    Clean up

--- GitHub/Classes/Definition/GitHubEndpoint.swift
+++ GitHub/Classes/Definition/GitHubEndpoint.swift
@@ -9,8 +9,10 @@ import Sweeft
 
 public enum GitHubEndpoint: String, APIEndpoint {
     case markdown
+    
     case gitignoreTemplates = "/gitignore/templates"
     case gitignore = "/gitignore/templates/{id}"
+    
     case grants = "applications/grants"
     case authorizations = "authorizations"
     case organizations = "orgs"
--- GitHub/Classes/Model/Authentication/Authorization.swift
+++ GitHub/Classes/Model/Authentication/Authorization.swift
@@ -8,12 +8,10 @@
 import Foundation
 
 struct Authorization: Codable {
-    
     let grant: Grant
     let token: String
     let hashedToken: String
     let note: String?
     let noteURL: URL?
     let fingerPrint: String
-    
 }
